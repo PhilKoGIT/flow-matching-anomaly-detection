23c5f58ecd82d20d38d0f44d3c069063beae5437
Philip Konz <philip.konz@students.uni-mannheim.de>
2025-11-30 14:29:02 +0100

    created completed main file

---
+++ tests/evaluater.py
@@ -0,0 +1,239 @@
+import random
+import os
+import torch
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+from ForestDiffusion import ForestDiffusionModel
+import pandas as pd
+from pathlib import Path
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score, precision_recall_curve, roc_curve, confusion_matrix
+import seaborn as sns
+import json
+import joblib
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import average_precision_score
+import time
+#copied partly from by utils.py of https://github.com/ZhongLIFR/TCCM-NIPS/blob/main/utils.py
+
+
+n_t = 5
+duplicate_K = 3
+number_of_runs = 5
+
+def load_dataset(dataset_name, semi_supervised):
+    if "business_dataset" in dataset_name:
+        if semi_supervised:
+            pass
+        else: 
+            pass
+    else: 
+        if semi_supervised:
+            base_dir = Path(__file__).resolve().parent
+            #file_path = base_dir.parent / "data" / "5_campaign.npz"
+            file_path = base_dir.parent / "data" / dataset_name
+            data = np.load(file_path, allow_pickle=True)
+            X, y = data['X'], data['y'].astype(int)
+            x_normal, X_anomalous = X[y == 0], X[y == 1]
+            y_normal, y_anomalous = y[y == 0], y[y == 1]
+
+            X_train, X_test_normal, y_train, y_test_normal = train_test_split(
+                x_normal, y_normal, test_size = 0.5, random_state = 42
+            )
+            # Test set contains both normal and abnormal data
+            X_test = np.vstack((X_test_normal, X_anomalous))
+            y_test = np.concatenate((y_test_normal, y_anomalous))
+            # Data standardization
+            scaler = StandardScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.transform(X_test)
+
+            # Print dataset information
+            print(" ")
+            print(f"Dataset loaded successfully!")
+            print(f"Training data: {X_train.shape}, Normal: {len(y_train)}")
+            print(f"Test data: {X_test.shape}, Normal: {sum(y_test == 0)}, Anomalies: {sum(y_test == 1)}")
+        else:
+            pass
+    return X_train, X_test, y_test
+
+
+def train_test_model(seed, X_train, X_test, y_test, n_t, duplicate_K):
+    # train time, dev time, rec time
+    times = []
+
+    start_time = time.time()
+    model = ForestDiffusionModel(
+        X=X_train,
+        label_y=None,     # unsupervised; wir geben Labels nur für Evaluation
+        # Diffusion settings
+        n_t=n_t,
+        duplicate_K=duplicate_K,
+        diffusion_type='flow',  # wichtig für compute_deviation_score
+        eps=1e-3,
+        model='xgboost',
+        max_depth=7,
+        n_estimators=100,
+        eta=0.3,
+        gpu_hist=False,   # auf True setzen, wenn GPU verfügbar
+        n_batch=25,        # Important: 0 for compute_deviation_score
+        seed=seed,
+        n_jobs=-1,
+        bin_indexes=[],
+        cat_indexes=[],
+        int_indexes=[],
+        remove_miss=False,
+        p_in_one=True,    # WICHTIG für compute_deviation_score
+
+    )
+    end_time_train = time.time()
+    time_train = end_time_train - start_time
+    times.append(time_train)
+    print("✓ Model trained successfully on full training data!")
+    joblib.dump(model, "campaign_model.joblib")
+
+    # ---Computation deviation Score ---
+    start_time_dev = time.time()
+    anomaly_scores_deviation = model.compute_deviation_score(
+        test_samples=X_test,
+        n_t=n_t   # same amount of noise as training
+    )
+    end_time_dev = time.time()
+    time_dev = end_time_dev - start_time_dev
+    times.append(time_dev)
+
+    # ---Computation reconstruction Score ---
+    start_time_rec = time.time()
+    anomaly_scores_reconstruction = model.compute_reconstruction_score(
+        test_samples=X_test,
+        n_t=n_t
+    )
+    end_time_rec = time.time()
+    time_reconstruction = end_time_rec - start_time_rec
+    times.append(time_reconstruction)
+
+    # ---calculate the auroc & auprc ---
+    auroc_deviation = roc_auc_score(y_test, anomaly_scores_deviation)
+    auprc_deviation = average_precision_score(y_test, anomaly_scores_deviation)
+    print(f"\nAUROC deviation: {auroc_deviation:.4f}")
+    print(f"AUPRC deviation (Average Precision): {auprc_deviation:.4f}")
+
+    auroc_reconstruction = roc_auc_score(y_test, anomaly_scores_reconstruction)
+    auprc_reconstruction = average_precision_score(y_test, anomaly_scores_reconstruction)
+    print(f"\nAUROC reconstruction: {auroc_reconstruction:.4f}")
+    print(f"AUPRC reconstruction (Average Precision): {auprc_reconstruction:.4f}")
+
+    return auroc_deviation, auprc_deviation, auroc_reconstruction, auprc_reconstruction, times
+
+
+##TIME !!!! 
+#noch mit binary, float etc. machen, als parameter übergeben!!!
+
+if __name__ == "__main__":
+    
+    dataset_name = ["5_campaign.npz"]
+
+    #schleife bauen
+    supervised = [True]
+
+    for dataset in dataset_name:
+        print("\n" + "=" * 80)
+        print(f"DATASET: {dataset}")
+        print("=" * 80)
+
+        dev_auroc_list = []
+        dev_auprc_list = []
+        rec_auroc_list = []
+        rec_auprc_list = []
+
+        time_train_list = []
+        time_dev_inf_list = []
+        time_rec_inf_list = []
+        #compute seperately total times
+        time_dev_total_list = [] 
+        time_rec_total_list = []
+
+        #always the same split for comparability!!!
+        X_train, X_test, y_test = load_dataset(dataset, semi_supervised=True)
+
+
+        for i in range(number_of_runs):
+            print(f"\n--- Iteration {i+1}/{number_of_runs} (Seed: {i}) ---")
+
+            a_dev, ap_dev, a_rec, ap_rec, times = train_test_model(
+                seed=i, 
+                X_train=X_train,
+                X_test=X_test,
+                y_test=y_test,
+                n_t=n_t,
+                duplicate_K=duplicate_K
+            )
+
+            print(f"Iteration {i+1} results: AUROC Deviation: {a_dev:.4f}, AUPRC Deviation: {ap_dev:.4f}, AUROC Reconstruction: {a_rec:.4f}, AUPRC Reconstruction: {ap_rec:.4f}")
+            dev_auroc_list.append(a_dev)
+            dev_auprc_list.append(ap_dev)
+            rec_auroc_list.append(a_rec)
+            rec_auprc_list.append(ap_rec)
+
+            #add times to time lists
+            time_train_list.append(times[0])
+            time_dev_inf_list.append(times[1])
+            time_rec_inf_list.append(times[2])
+
+            #compute seperately total times
+            time_dev_total_list.append(times[0] + times[1])  
+            time_rec_total_list.append(times[0] + times[2])  
+
+        dev_auroc_mean = np.mean(dev_auroc_list)
+        dev_auroc_std = np.std(dev_auroc_list)
+        dev_auprc_mean = np.mean(dev_auprc_list)
+        dev_auprc_std = np.std(dev_auprc_list)
+
+        rec_auroc_mean = np.mean(rec_auroc_list)
+        rec_auroc_std = np.std(rec_auroc_list)
+        rec_auprc_mean = np.mean(rec_auprc_list)
+        rec_auprc_std = np.std(rec_auprc_list)
+
+        #compute the time means and stds
+        time_train_mean = np.mean(time_train_list)
+        time_train_std = np.std(time_train_list)
+        time_dev_inf_mean = np.mean(time_dev_inf_list)
+        time_dev_inf_std = np.std(time_dev_inf_list)
+        time_rec_inf_mean = np.mean(time_rec_inf_list)
+        time_rec_inf_std = np.std(time_rec_inf_list)
+
+        time_dev_total_mean = np.mean(time_dev_total_list)
+        time_dev_total_std = np.std(time_dev_total_list)
+        time_rec_total_mean = np.mean(time_rec_total_list)
+        time_rec_total_std = np.std(time_rec_total_list)
+
+
+        print("\n" + "=" * 40 + " FINAL RESULTS " + "=" * 40)
+        print(f"Dataset: {dataset}")
+        print("dev_auroc_list: "+str(dev_auroc_list))
+        print("dev_auprc_list: "+str(dev_auprc_list))
+        print("rec_auroc_list: "+str(rec_auroc_list))
+        print("rec_auprc_list: "+str(rec_auprc_list))
+        print("-" * 87)
+        print(f"Metric | Deviation Score (Mean ± Std) | Reconstruction Score (Mean ± Std)")
+        print("-" * 87)
+        print(f"AUROC | {dev_auroc_mean:.3f} ± {dev_auroc_std:.3f} | {rec_auroc_mean:.3f} ± {rec_auroc_std:.3f}")
+        print(f"AUPRC | {dev_auprc_mean:.3f} ± {dev_auprc_std:.3f} | {rec_auprc_mean:.3f} ± {rec_auprc_std:.3f}")
+        print("=" * 87)
+
+        print("\n" + "=" * 40 + " TIME RESULTS " + "=" * 40)
+        print(f"Dataset: {dataset}")
+        print("-" * 87)
+        print(f"Time Metric | Training Time (Mean ± Std) | Dev Inference Time (Mean ± Std) | Rec Inference Time (Mean ± Std)")
+        print("-" * 87)
+        print(f"Time (s) | {time_train_mean:.3f} ± {time_train_std:.3f} | {time_dev_inf_mean:.3f} ± {time_dev_inf_std:.3f} | {time_rec_inf_mean:.3f} ± {time_rec_inf_std:.3f}")
+        print(f"Total Time Dev (s) | {time_dev_total_mean:.3f} ± {time_dev_total_std:.3f}")
+        print(f"Total Time Rec (s) | {time_rec_total_mean:.3f} ± {time_rec_total_std:.3f}")
+        print("=" * 87) 
+
+
+
