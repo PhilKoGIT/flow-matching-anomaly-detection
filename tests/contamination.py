321d198fb8647b860e25fc909e7fa2794a9316aa
Philip Konz <philip.konz@gmx.de>
2025-12-08 23:00:47 +0100

    push safe

--- tests/contamination.py
+++ tests/contamination.py
@@ -475,7 +475,7 @@ def plot_score_models_comparison(all_results, score, metric, dataset_names, mode
 
     # For each model name a color
     #colors = {"ForestDiffusion_1": "blue", "TCCM": "green", "ForestFlow_1": "red"}
-    colors = {"TCCM_n_t_20": "red", "TCCM_n_t_10": "green"}
+    colors = {"TCCM_n_t_20": "red", "TCCM_n_t_300": "green"}
     for idx, dataset_name in enumerate(dataset_names):
         ax = axs[idx] if len(dataset_names) > 1 else axs
         
@@ -586,27 +586,27 @@ if __name__ == "__main__":
         #         "diffusion_type": "flow"
         #     }
         # },
-        # "ForestDiffusion_2": {
-        #     "type": "forest",
+        "ForestDiffusion_2": {
+            "type": "forest",
+            "params": {
+                "n_t": 20,
+                "duplicate_K": 5,
+                "duplicate_K_test": 5,
+                "diffusion_type": "vp"
+            }
+        },
+        # "TCCM_n_t_20": {
+        #     "type": "tccm",
         #     "params": {
-        #         "n_t": 40,
-        #         "duplicate_K": 5,
-        #         "duplicate_K_test": 5,
-        #         "diffusion_type": "flow"
+        #         "n_t": 20
         #     }
-        # },
-        "TCCM_n_t_20": {
-            "type": "tccm",
-            "params": {
-                "n_t": 20
-            }
-        }, 
-        "TCCM_n_t_60": {
-            "type": "tccm",
-            "params": {
-                "n_t": 10
-            }
-        }
+        # }, 
+        # "TCCM_n_t_300": {
+        #     "type": "tccm",
+        #     "params": {
+        #         "n_t": 3
+        #     }
+        # }
      }
     
     all_results_combined = {}
@@ -619,6 +619,7 @@ if __name__ == "__main__":
         assert params.get("duplicate_K") == params.get("duplicate_K_test"), "duplicate_K and duplicate_K_test must be the same, for simplicity"
         if model_type == "forest":
             if params["diffusion_type"] == "vp":
+ #               score_list = ["deviation", "decision"]
                 score_list = ["deviation", "decision"]
             else:
                 score_list = ["deviation", "reconstruction", "decision"]
